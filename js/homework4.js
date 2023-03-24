794f74fccbfa99d195c39c3024990072222c6eb4
ViStef1337 <stefanykvitalik1337@gmail.com>
2023-03-24 17:58:22 -0500

    homework4 bug fixed

--- js/homework4.js
+++ js/homework4.js
@@ -1,7 +1,7 @@
 'use strict'
 let num1 = Number(prompt ('enter first number: '));
 let num2 = Number(prompt ('enter second number: '));
-let operator = Number(prompt ('enter operator: '));
+let operator = prompt ('enter operator: ');
 let result;
 if(operator == '+') {
     result = num1 + num2;
